d184ccc284e6466488500856b5be52d38d17add1
NotYeshwanthReddy <peddamalluyeshwanthreddy@gmail.com>
2020-09-26 17:24:01 +0530

    UI Created....!

---
+++ config.js
@@ -0,0 +1,69 @@
+
+let colorsSizes = {
+    "1": {
+        size: 64,
+        color: "#FFF999"
+    },
+    "2": {
+        size: 64,
+        color: "#FFF999"
+    },
+    "3": {
+        size: 64,
+        color: "#FFF999"
+    },
+    "4": {
+        size: 64,
+        color: "#FFF999"
+    },
+    "5": {
+        size: 64,
+        color: "#CCF9AA"
+    },
+    "6": {
+        size: 64,
+        color: "#CCF9AA"
+    },
+    "7": {
+        size: 64,
+        color: "#CCF9AA"
+    },
+    "8": {
+        size: 64,
+        color: "#CCF9AA"
+    },
+    "9": {
+        size: 64,
+        color: "#49BB6C"
+    },
+    "10": {
+        size: 64,
+        color: "#49BB6C"
+    },
+    "11": {
+        size: 64,
+        color: "#49BB6C"
+    },
+
+    "12": {
+        size: 64,
+        color: "#49BB6C"
+    },
+    "13": {
+        size: 64,
+        color: "#2494C1"
+    },
+    "14": {
+        size: 64,
+        color: "#2494C1"
+    },
+    "15": {
+        size: 64,
+        color: "#2494C1"
+    },
+
+    "0": {
+        size: 64,
+        color: "#FFFFFF"
+    },
+}
---
+++ game_logic.js
@@ -0,0 +1,51 @@
+var grid;
+var moves = 0;
+
+// Setup blank Canvas with Grid
+function setup() {
+    createCanvas(400, 400);
+    noLoop();
+    grid = blankGrid();
+
+    updateCanvas();
+}
+
+// Returns solved Grid
+function blankGrid() {
+    return [[1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 0]]
+        ;
+}
+
+
+function updateCanvas() {
+    background(250);
+    drawGrid();
+}
+
+// Draws the given grid over canvas
+function drawGrid() {
+    var w = 100;
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            noFill();
+            strokeWeight(2);
+            let val = grid[j][i];
+            let s = val.toString();
+
+            stroke(0);
+            fill(colorsSizes[s].color);
+
+            rect(i * w, j * w, w, w);
+            if (grid[i][j] !== 0) {
+                textAlign(CENTER, CENTER);
+                noStroke();
+                fill(0);
+                textSize(colorsSizes[s].size);
+                text(val, i * w + w / 2, j * w + w / 2);
+            }
+        }
+    }
+}
