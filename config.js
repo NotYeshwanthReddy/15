bb97a6818e9fbc8d3600e9d659eec00de744c68d
NotYeshwanthReddy <peddamalluyeshwathreddy@gmail.com>
2020-09-27 01:44:51 +0530

    Colors added

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
-let dataColors=[
+let dataColors = [
     ["#FFFFFF", "#E5A8FF", "#C873FF", "#A736FF", "#9000FF"],
     ["#FFFFFF", "#FFD180", "#FFAB40", "#FF9100", "#FF6D00"],
     ["#FFFFFF", "#A7FFEB", "#64FFDA", "#1DE9B6", "#00BFA5"],
+    ["#FFFFFF", "#FF8A80", "#FF5252", "#FF1744", "#D50000"],
+    ["#FFFFFF", "#B9F6CA", "#69F0AE", "#00E676", "#00C853"],
+    ["#FFFFFF", "#CCFF90", "#B2FF59", "#76FF03", "#64DD17"],
+    ["#FFFFFF", "#FFE57F", "#FFD740", "#FFC400", "#FFAB00"],
+    ["#FFFFFF", "#CFD8DC", "#90A4AE", "#607D8B", "#455A64"],
     ["#FFFFFF", "#80D8FF", "#40C4FF", "#00B0FF", "#0091EA"]
 ]
